8e567a39558cb8c36d0aec97d21521efb6d56808
Rjoydip <joydipand@gmail.com>
2018-04-14 23:01:09 +0545

    done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-
-    let x = 1;
-    let y = 3;
-
-    match (x, y) {
-        (x,y) if x > y => { println!("Decreasing"); }
-        (x,y) if y > x => { println!("Increasing"); }
-        _              => { println!("Equal")}
-    }
+   
 }
